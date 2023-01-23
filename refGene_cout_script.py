e45777ea27804947bac2cf739f72e30619de3b83
Nakamura <106055493+knakamura6222053@users.noreply.github.com>
2023-01-23 15:23:23 +0900

    Update refGene_cout_script.py

--- refGene_cout_script.py
+++ refGene_cout_script.py
@@ -1,61 +1,30 @@
 #!/usr/bin/env/python3
 
-#　ファイルやディレクトリの操作のosモジュールのインポート
-#　対応する文字列のインポート
-#　csvモジュールのインポート
-#　operatorモジュールのインポート
-#  コマンドライン引数のインポート
-
-import os
-import re
-import csv
-import operator
-import sys
-
-count= {}
-args = sys.argv
-
-print(args[0])
-print(args[1])
-
-
-# refGene.txtから取り出したい要素の変数を設定
-
-file = 'refGene.txt'
-pattern = args[1]
-
-
-with open('refGene.txt', mode = 'r')as f:
-    data = f.readlines()
-
-#GREP_TARGETでpatternにマッチしたものをまとめる。
-
-GREP_TARGET = [line for line in data if pattern in line]
-
-#マッチしたものを新しいtxtに記述する。
-
-with open('args[1].txt', mode = 'w')as f:
-    f.writelines(GREP_TARGET)
-
-#txtを読み込み、csv形式に直す。
-
-with open('args[1].txt', mode = 'r')as f:
-    csvdata = csv.reader(f,delimiter='\t')
-    result = sorted(csvdata,key=operator.itemgetter(1))
-
-# 該当の列(AccessionとGene)のものを抽出
-
-    for row in result:
-        Accession = row[1]
-
-#　該当のものがあればカウントしていく。
-
-        count.setdefault(Accession, 0)
-        count[Accession] +=1
-for key, value in count.items():
-    print('{}: {}'.format(key, value)) 
-
-
-
-
+# Open the refGene.txt file 
+with open("refGene.txt", "r") as file:
+    
+    # Create a dictionary to store the gene name and count
+    gene_counts = {}
+    
+    # Iterate through each line in the file
+    for line in file:
+        
+        # Split the line by tab character
+        columns = line.strip().split("\t")
+        
+        # Get the gene name from the second column
+        gene_name = columns[1]
+        
+        # Check if the gene name is already in the dictionary
+        if gene_name in gene_counts:
+            # If it is, increment the count
+            gene_counts[gene_name] += 1
+        else:
+            
+            # If it's not, add it to the dictionary with a count of 1
+            gene_counts[gene_name] = 1
+    
+    # Print out the gene name and count for each gene
+    for gene_name, count in gene_counts.items():
+        print(gene_name, count)
 
