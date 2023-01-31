9d8fdec9b6cb23b5fb43ba86d523f526fa18ef45
Nakamura <106055493+knakamura6222053@users.noreply.github.com>
2023-01-31 11:18:52 +0900

    Update refGene_cout_script.py

--- refGene_cout_script.py
+++ refGene_cout_script.py
@@ -23,8 +23,16 @@ with open("refGene.txt", "r") as file:
             
             # If it's not, add it to the dictionary with a count of 1
             gene_counts[gene_name] = 1
+# sort result
+sorted_gene_counts =sorted(gene_counts.items(), key = lambda x: x[1], reverse=True)
+
+# output 
+with open("Accession_count.txt","w")as output:
+    for gene_name, count in sorted_gene_counts:
+        output.write(f"Gene:{gene_name} \nCount:{count}\n")
     
-    # Print out the gene name and count for each gene
-    for gene_name, count in gene_counts.items():
-        print(gene_name, count)
+    
+# Print out the gene name and count for each gene
+#for gene_name, count in gene_counts.items():
+#   print(gene_name, count)
 
