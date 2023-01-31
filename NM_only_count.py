7dd67a29fb027cf590ebbc226e3114038176d2ee
Nakamura <106055493+knakamura6222053@users.noreply.github.com>
2023-01-31 18:41:58 +0900

    Update NM_only_count.py

--- NM_only_count.py
+++ NM_only_count.py
@@ -1,45 +1,31 @@
 #!/usr/bin/env/python3
 
-# refGene.txtを読み込む
-with open("refGene.txt", "r")as file:
-    
-# gene_nameとcountをする際の枠を作る。(dict型)
-    d_gene_counts = {}
+# read refGene.txt file
+with open("refGene.txt") as file:
+    data = file.readlines()
 
-# 1列ごとにデータを繰り返し読み込む
-    for line in file:
-        
-# 列ごとの空白除去してタブ区切りにしてカラムに入れる
-        columns = line.strip().split("\t")
+# create a dictionary to store gene name and its count
+gene_count = {}
 
-# refGene.txtのgenomeをカラムに入れていく 
-        gene_name = columns[1]
-       # accession番号がNMから始まるもの
-        if not gene_name.startswith("NM"):
-            continue
-
-# d_gene_counts内にgene_nameがすでに入っている場合(複数)
-        if gene_name in d_gene_counts:
-            d_gene_counts[gene_name] += 1
-
-
+# loop through each line in the file
+for line in data:
+    items = line.strip().split("\t")
+    accession = items[1]
+    gene_name = items[12]
+    # check if accession number starts with "NM"
+    if accession.startswith("NM"):
+        # add gene name to dictionary or increase its count
+        if gene_name in gene_count:
+            gene_count[gene_name] += 1
         else:
-            d_gene_counts[gene_name] = 1
+            gene_count[gene_name] = 1
 
-# 結果のソート
-#sorted_d_gene_counts = sorted(d_gene_counts.items(), key = lambda x: x[1], reverse=True)
-
-# 結果出力(NM番号を取り出してsort)
+# output
 with open("only_NM_count.txt","w")as output:
-    for gene_name in sorted(d_gene_counts.keys()):
-        count = d_gene_counts[gene_name]
-        output.write(f"{gene_name}\t{count}\n")
+    for gene_name in sorted(gene_count.keys()):
+        count = gene_count[gene_name]
+        output.write(f"{gene_name} \t{count}\n")
 
 
 
 
-## 出力する
-#for gene_name,count in sorted_d_gene_counts:
-#    print(gene_name,count)
-        
-             
